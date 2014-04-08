392f8250858a20df8603d8ce4efbc2433b95f348
PBBT05791 <PBBT05791@P64L03BID3N.COMMUN.AD.SNCF.FR>
2014-04-08 17:17:12 +0200

    dev

---
+++ application/views/leaves/counters.php
@@ -0,0 +1,22 @@
+
+<table class="table table-bordered table-hover">
+<thead>
+    <tr>
+      <th>Leave type</th>
+      <th>Taken</th>
+      <th>Entitled</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td><?php echo $summary[0][0]; ?></td>
+      <td>...</td>
+      <td>...</td>
+    </tr>
+    <tr>
+      <td><?php echo $summary[0][1]; ?></td>
+      <td>...</td>
+      <td>...</td>
+    </tr>
+  </tbody>
+</table>

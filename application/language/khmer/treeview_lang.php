d035f48f4573b52c8a94d4e439c1a39e602eb087
bbalet <benjamin.balet@gmail.com>
2014-06-19 11:06:26 +0200

    BF:tooltip "link to documentation" was hard-coded in view users/create. ER:Add the support of khmer language in the WebUI and pages (translation progress is 20%).

--- application/language/khmer/treeview_lang.php
+++ application/language/khmer/treeview_lang.php
@@ -16,10 +16,10 @@
  * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-$lang['treeview_context_menu_create'] = 'Create';
-$lang['treeview_context_menu_rename'] = 'Rename';
-$lang['treeview_context_menu_remove'] = 'Remove';
+$lang['treeview_context_menu_create'] = 'បង្កើត';
+$lang['treeview_context_menu_rename'] = 'ប្តូរឈ្មោះ';
+$lang['treeview_context_menu_remove'] = 'ដកចេញ';
 $lang['treeview_context_menu_edit'] = 'កែសម្រួល';
-$lang['treeview_context_menu_copy'] = 'Copy';
-$lang['treeview_context_menu_cut'] = 'Cut';
-$lang['treeview_context_menu_paste'] = 'Paste';
+$lang['treeview_context_menu_copy'] = 'ចម្លងតាម';
+$lang['treeview_context_menu_cut'] = 'កាត់ចេញ';
+$lang['treeview_context_menu_paste'] = 'ផ្តិត';

0e51d257ccc4591e52640939f457a54f8c797418
MARIE-MARTHE Cédric <cedric.cmm@gmail.com>
2014-11-03 15:21:52 +0100

    Update extra.php
    remove hardcoded from email do.not@reply.me

--- application/controllers/extra.php
+++ application/controllers/extra.php
@@ -232,7 +232,7 @@ class Extra extends CI_Controller {
                 $this->email->phpmailer->Encoding = 'quoted-printable';
             }
 
-            $this->email->from('do.not@reply.me', 'LMS');
+            $this->email->from($this->config->item('from_mail'), $this->config->item('from_name'));
             $this->email->to($manager['email']);
             $this->email->subject(lang('email_extra_request_reject_subject') .
                     $this->session->userdata('firstname') . ' ' .

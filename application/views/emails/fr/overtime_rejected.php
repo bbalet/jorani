6c633cd5173a2575eabbd3afc6363f181865684b
neilime <neilime@users.noreply.github.com>
2015-05-21 14:49:16 +0200

    Add greating text for women

--- application/views/emails/fr/overtime_rejected.php
+++ application/views/emails/fr/overtime_rejected.php
@@ -25,7 +25,7 @@
     </head>
     <body>
         <h3>{Title}</h3>
-        Cher {Firstname} {Lastname}, <br />
+        Cher/Chère {Firstname} {Lastname}, <br />
         <br />
         La déclaration d'heures supplémentaires que vous avez soumise a été refusée. Voici les détails :
         <table border="0">

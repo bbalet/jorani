78e77fd2995ac342a791261264031203a1278f52
bbalet <benjamin.balet@gmail.com>
2015-09-02 17:08:45 +0200

    ER:Experimental export of tabular calendar (WIP)

--- application/controllers/calendar.php
+++ application/controllers/calendar.php
@@ -237,4 +237,241 @@ class Calendar extends CI_Controller {
             $this->load->view('templates/footer');
         }
     }
+    
+    /**
+     * Export the tabular calendar into Excel. The presentation differs a bit according to the limitation of Excel
+     * We'll get one line for the morning and one line for the afternoon
+     * @author Benjamin BALET <benjamin.balet@gmail.com>
+     */
+    public function tabular_export($id=-1, $month=0, $year=0, $children=TRUE) {
+        expires_now();
+        
+        //Load the language file (the loaded language depends if it was called from the public view)
+        if (($this->config->item('public_calendar') == TRUE) && (!$this->session->userdata('logged_in'))) {
+            $this->load->library('polyglot');;
+            $language = $this->config->item('language');
+            //$language_code =  $this->polyglot->language2code($language);
+        } else {
+            setUserContext($this);
+            $language = $this->language;
+        }
+        $this->lang->load('calendar', $language);
+        $this->lang->load('global', $language);
+        
+        $this->load->library('excel');
+        $sheet = $this->excel->setActiveSheetIndex(0);
+        
+
+        //Print the header with the values of the export parameters
+        $sheet->setTitle(lang('calendar_tabular_export_title'));
+        $sheet->setCellValue('A1', lang('calendar_tabular_export_param_entity'));
+        $sheet->setCellValue('A2', lang('calendar_tabular_export_param_month'));
+        $sheet->setCellValue('A3', lang('calendar_tabular_export_param_year'));
+        $sheet->setCellValue('A4', lang('calendar_tabular_export_param_children'));
+        $sheet->getStyle('A1:A4')->getFont()->setBold(true);
+        $this->load->model('organization_model');
+        $sheet->setCellValue('B1', $this->organization_model->get_label($id));
+        $sheet->setCellValue('B2', $month);
+        $sheet->setCellValue('B3', $year);
+        if ($children == TRUE) {
+            $sheet->setCellValue('B4', lang('global_true'));
+        } else {
+            $sheet->setCellValue('B4', lang('global_false'));
+        }
+
+        //Print two lines : the short name of all days for the selected month (horizontally aligned)
+        $start = $year . '-' . $month . '-' . '1';    //first date of selected month
+        $lastDay = date("t", strtotime($start));    //last day of selected month
+        for ($ii = 1; $ii <=$lastDay; $ii++) {
+            $dayNum = date("N", strtotime($year . '-' . $month . '-' . $ii));
+            $col = $this->excel->column_name(3 + $ii);
+            //Print day number
+            $sheet->setCellValue($col . '9', $ii);
+            //Print short name of the day
+            switch ($dayNum)
+            {
+                case 1: $sheet->setCellValue($col . '8', lang('calendar_monday_short')); break;
+                case 2: $sheet->setCellValue($col . '8', lang('calendar_tuesday_short')); break;
+                case 3: $sheet->setCellValue($col . '8', lang('calendar_wednesday_short')); break;
+                case 4: $sheet->setCellValue($col . '8', lang('calendar_thursday_short')); break;
+                case 5: $sheet->setCellValue($col . '8', lang('calendar_friday_short')); break;
+                case 6: $sheet->setCellValue($col . '8', lang('calendar_saturday_short')); break;
+                case 7: $sheet->setCellValue($col . '8', lang('calendar_sunday_short')); break;
+            }
+        }
+        //Label for employee name
+        $sheet->setCellValue('C8', lang('calendar_tabular_export_thead_employee'));
+        $sheet->mergeCells('C8:C9');
+        //The header is horizontally aligned
+        $col = $this->excel->column_name(3 + $lastDay);
+        $sheet->getStyle('C8:' . $col . '9')->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
+        
+        //Get the tabular data
+        $this->load->model('leaves_model');
+        $tabular = $this->leaves_model->tabular($id, $month, $year, $children);
+        
+        //Box around the lines for each employee
+        $styleBox = array(
+            'borders' => array(
+                'top' => array(
+                    'style' => PHPExcel_Style_Border::BORDER_THIN
+                ),
+                'bottom' => array(
+                    'style' => PHPExcel_Style_Border::BORDER_THIN
+                )
+            )
+          );
+        
+        //Background colors for the calendar according to the type of leave
+        $styleBgPlanned = array(
+            'fill' => array(
+                'type' => PHPExcel_Style_Fill::FILL_SOLID,
+                'color' => array('rgb' => 'DDD')
+            )
+        );
+        $styleBgRequested = array(
+            'fill' => array(
+                'type' => PHPExcel_Style_Fill::FILL_SOLID,
+                'color' => array('rgb' => 'F89406')
+            )
+        );
+        $styleBgAccepted = array(
+            'fill' => array(
+                'type' => PHPExcel_Style_Fill::FILL_SOLID,
+                'color' => array('rgb' => '468847')
+            )
+        );
+        $styleBgRejected = array(
+            'fill' => array(
+                'type' => PHPExcel_Style_Fill::FILL_SOLID,
+                'color' => array('rgb' => 'FF0000')
+            )
+        );
+        $styleBgDayOff = array(
+            'fill' => array(
+                'type' => PHPExcel_Style_Fill::FILL_SOLID,
+                'color' => array('rgb' => '000000')
+            )
+        );
+        
+        //$sheet->getComment('E1')->getText()->createTextRun(lang('Requested'));
+        
+        $line = 10;
+        //Iterate on all employees of the selected entity
+        foreach ($tabular as $employee) {
+            //Merge the two line containing the name of the employee and apply a border around it
+            $sheet->setCellValue('C' . $line, $employee->name);
+            $sheet->mergeCells('C' . $line . ':C' . ($line + 1));
+            $col = $this->excel->column_name($lastDay + 3);
+            $sheet->getStyle('C' . $line . ':' . $col . ($line + 1))->applyFromArray($styleBox);
+            
+            //Iterate on all days of the selected month
+            $dayNum = 0;
+            foreach ($employee->days as $day) {
+                $dayNum++;
+                $col = $this->excel->column_name(3 + $dayNum);
+                $overlapping = FALSE;
+                if (strstr($day->display, ';')) {//Two statuses in the cell
+                    $periods = explode(";", $day->display);
+                    $statuses = explode(";", $day->status);
+                    $types = explode(";", $day->type);
+                        //0 - Working day  _
+                        //1 - All day           []
+                        //2 - Morning        |\
+                        //3 - Afternoon      /|
+                        //4 - All Day Off       []
+                        //5 - Morning Day Off   |\
+                        //6 - Afternoon Day Off /|
+                      $sheet->getComment($col . $line)->getText()->createTextRun($types[0]);
+                      $sheet->getComment($col . ($line + 1))->getText()->createTextRun($types[1]);
+                      switch (intval($statuses[0]))
+                      {
+                        case 1: $sheet->getStyle($col . $line)->applyFromArray($styleBgPlanned); break;  // Planned
+                        case 2: $sheet->getStyle($col . $line)->applyFromArray($styleBgRequested); break;  // Requested
+                        case 3: $sheet->getStyle($col . $line)->applyFromArray($styleBgAccepted); break;  // Accepted
+                        case 4: $sheet->getStyle($col . $line)->applyFromArray($styleBgRejected); break;  // Rejected
+                        case '5': $sheet->getStyle($col . $line)->applyFromArray($styleBgDayOff); break;    //Day off
+                        case '6': $sheet->getStyle($col . $line)->applyFromArray($styleBgDayOff); break;    //Day off
+                      }
+                      switch (intval($statuses[1]))
+                      {
+                        case 1: $sheet->getStyle($col . ($line + 1))->applyFromArray($styleBgPlanned); break;  // Planned
+                        case 2: $sheet->getStyle($col . ($line + 1))->applyFromArray($styleBgRequested); break;  // Requested
+                        case 3: $sheet->getStyle($col . ($line + 1))->applyFromArray($styleBgAccepted); break;  // Accepted
+                        case 4: $sheet->getStyle($col . ($line + 1))->applyFromArray($styleBgRejected); break;  // Rejected
+                        case '5': $sheet->getStyle($col . ($line + 1))->applyFromArray($styleBgDayOff); break;    //Day off
+                        case '6': $sheet->getStyle($col . ($line + 1))->applyFromArray($styleBgDayOff); break;    //Day off
+                      }//Two statuses in the cell
+                } else {//Only one status in the cell
+                    switch ($day->display) {
+                        case '1':   //All day
+                                $sheet->getComment($col . $line)->getText()->createTextRun($day->type);
+                                $sheet->getComment($col . ($line + 1))->getText()->createTextRun($day->type);
+                                switch ($day->status)
+                                {
+                                    // 1 : 'Planned';
+                                    // 2 : 'Requested';
+                                    // 3 : 'Accepted';
+                                    // 4 : 'Rejected';
+                                    case 1: $sheet->getStyle($col . $line . ':' . $col . ($line + 1))->applyFromArray($styleBgPlanned); break;  // Planned
+                                    case 2: $sheet->getStyle($col . $line . ':' . $col . ($line + 1))->applyFromArray($styleBgRequested);            // Requested
+                                    case 3: $sheet->getStyle($col . $line . ':' . $col . ($line + 1))->applyFromArray($styleBgAccepted); break;  // Accepted
+                                    case 4: $sheet->getStyle($col . $line . ':' . $col . ($line + 1))->applyFromArray($styleBgRejected); break;  // Rejected
+                                }
+                                break;
+                        case '2':   //AM
+                            $sheet->getComment($col . $line)->getText()->createTextRun($day->type);
+                            switch ($day->status)
+                              {
+                                  case 1: $sheet->getStyle($col . $line)->applyFromArray($styleBgPlanned); break;  // Planned
+                                  case 2: $sheet->getStyle($col . $line)->applyFromArray($styleBgRequested); break;  // Requested
+                                  case 3: $sheet->getStyle($col . $line)->applyFromArray($styleBgAccepted); break;  // Accepted
+                                  case 4: $sheet->getStyle($col . $line)->applyFromArray($styleBgRejected); break;  // Rejected
+                              }
+                            break;
+                        case '3':   //PM
+                            $sheet->getComment($col . ($line + 1))->getText()->createTextRun($day->type);
+                            switch ($day->status)
+                              {
+                                  case 1: $sheet->getStyle($col . ($line + 1))->applyFromArray($styleBgPlanned); break;  // Planned
+                                  case 2: $sheet->getStyle($col . ($line + 1))->applyFromArray($styleBgRequested); break;  // Requested
+                                  case 3: $sheet->getStyle($col . ($line + 1))->applyFromArray($styleBgAccepted); break;  // Accepted
+                                  case 4: $sheet->getStyle($col . ($line + 1))->applyFromArray($styleBgRejected); break;  // Rejected
+                              }
+                            break;
+                        case '4': //Full day off
+                            $sheet->getStyle($col . $line . ':' . $col . ($line + 1))->applyFromArray($styleBgDayOff);
+                            $sheet->getComment($col . $line)->getText()->createTextRun($day->type);
+                            $sheet->getComment($col . ($line + 1))->getText()->createTextRun($day->type);
+                            break;
+                        case '5':  //AM off
+                            $sheet->getStyle($col . $line)->applyFromArray($styleBgDayOff);
+                            $sheet->getComment($col . $line)->getText()->createTextRun($day->type);
+                            break;
+                        case '6':   //PM off
+                            $sheet->getStyle($col . ($line + 1))->applyFromArray($styleBgDayOff);
+                            $sheet->getComment($col . ($line + 1))->getText()->createTextRun($day->type);
+                            break;
+                    }
+                  }//Only one status in the cell
+            }//day
+            $line += 2;
+        }//Employee
+        
+        //Autofit for all column containing the days
+        for ($ii = 1; $ii <=$lastDay; $ii++) {
+            $col = $this->excel->column_name($ii + 3);
+            $sheet->getColumnDimension($col)->setAutoSize(TRUE);
+        }
+        $sheet->getColumnDimension('A')->setAutoSize(TRUE);
+        $sheet->getColumnDimension('B')->setAutoSize(TRUE);
+        $sheet->getColumnDimension('C')->setWidth(40);
+        
+        $filename = 'tabular.xlsx';
+        header('Content-Type: application/vnd.ms-excel');
+        header('Content-Disposition: attachment;filename="' . $filename . '"');
+        header('Cache-Control: max-age=0');
+        $objWriter = PHPExcel_IOFactory::createWriter($this->excel, 'Excel2007');
+        $objWriter->save('php://output');
+    }
 }

c82af78c0b69851c2deee55e40d48bfcae7ab62f
bbalet <benjamin.balet@gmail.com>
2015-10-08 15:22:46 +0200

    Init v0.5.0

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
 <html lang="<?php echo $language_code;?>">
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta charset="utf-8">
     <title><?php echo $title ?> - Jorani</title>
-    <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
-    <meta charset="UTF-8">
     <meta description="Jorani a free and open source leave management system. Workflow of approval; e-mail notifications; calendars; reports; export to Excel and more.">
     <link href="<?php echo base_url();?>assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link href="<?php echo base_url();?>assets/bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">

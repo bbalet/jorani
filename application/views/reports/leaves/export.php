4c316dc8f29d28935f8e98f4519d8cc858d3ef89
Benjamin BALET <benjamin.balet@gmail.com>
2015-12-16 17:01:37 +0100

    ER:Update of phpDoc

--- application/views/reports/leaves/export.php
+++ application/views/reports/leaves/export.php
@@ -1,26 +1,9 @@
-<?php if (!defined('BASEPATH')) { exit('No direct script access allowed'); }
-/*
- * This file is part of Jorani.
- *
- * Jorani is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * lms is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Jorani.  If not, see <http://www.gnu.org/licenses/>.
- */
-
+<?php
 /**
  * This view exports to Excel the native report listing the approved leave requests of employees attached to an entity.
  * This report is launched by the user from the view reports/leaves.
- * @copyright  Copyright (c) 2014 - 2015 Benjamin BALET
- * @license      http://opensource.org/licenses/GPL-3.0 GPL-3.0
+ * @copyright  Copyright (c) 2014-2015 Benjamin BALET
+ * @license      http://opensource.org/licenses/AGPL-3.0 AGPL-3.0
  * @link            https://github.com/bbalet/jorani
  * @since         0.4.3
  */

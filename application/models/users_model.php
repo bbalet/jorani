b54105791497f19cc894dc6b36081f9478b8bc38
Benjamin BALET <benjamin.balet@gmail.com>
2017-01-05 13:47:26 +0100

    BF:Fix default behaviour for field users/datehired

--- application/models/users_model.php
+++ application/models/users_model.php
@@ -187,7 +187,7 @@ class Users_model extends CI_Model {
         if ($this->input->post('position') !== FALSE && $this->input->post('position') != '') {
             $data['position'] = $this->input->post('position');
         }
-        if ($this->input->post('datehired') !== FALSE && $this->input->post('datehired') == "") {
+        if ($this->input->post('datehired') !== FALSE && $this->input->post('datehired') != '') {
             $data['datehired'] = $this->input->post('datehired');
         }
         
@@ -326,7 +326,7 @@ class Users_model extends CI_Model {
         if ($this->input->post('position') !== FALSE && $this->input->post('position') != '') {
             $data['position'] = $this->input->post('position');
         }
-        if ($this->input->post('datehired') !== FALSE && $this->input->post('datehired') == "") {
+        if ($this->input->post('datehired') !== FALSE && $this->input->post('datehired') != '') {
             $data['datehired'] = $this->input->post('datehired');
         }
         if ($this->config->item('ldap_basedn_db')) {

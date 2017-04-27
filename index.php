b363a03697b5f3fc069867e456a9b57629bde609
Benjamin BALET <benjamin.balet@gmail.com>
2017-04-27 17:05:42 +0200

    WIP:Migration to CI3. Report the code that forces refresh from index

--- index.php
+++ index.php
@@ -305,6 +305,14 @@ switch (ENVIRONMENT)
 
 	define('VIEWPATH', $view_folder.DIRECTORY_SEPARATOR);
 
+        
+//make sure the PHP scripts are reloaded every time
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");   // Date in the past
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");  // always modified
+header("Cache-Control: no-store, no-cache, must-revalidate");   // HTTP/1.1
+header("Cache-Control: post-check=0, pre-check=0");  // HTTP/1.0
+header("Pragma: no-cache");
+        
 /*
  * --------------------------------------------------------------------
  * LOAD THE BOOTSTRAP FILE

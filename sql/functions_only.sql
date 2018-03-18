bd141cf199ee182a80a58a1788a956720c01f907
Benjamin BALET <benjamin.balet@gmail.com>
2018-03-18 15:32:06 +0700

    ER:List MySQL routines in a separated sql file

---
+++ sql/functions_only.sql
@@ -0,0 +1,144 @@
+-- ---------------------------------------------------
+-- Jorani MySQL functions
+-- You may need this file if youhave forgotten to backup routines
+--
+-- @license    http://opensource.org/licenses/AGPL-3.0 AGPL-3.0
+-- @copyright  Copyright (c) 2014-2018 Benjamin BALET
+
+DROP FUNCTION IF EXISTS `GetAncestry`;
+DELIMITER $$
+CREATE FUNCTION `GetAncestry`(GivenID INT) RETURNS varchar(1024) CHARSET utf8
+    NOT DETERMINISTIC
+    READS SQL DATA
+    SQL SECURITY INVOKER
+BEGIN
+    DECLARE rv VARCHAR(1024);
+    DECLARE cm CHAR(1);
+    DECLARE ch INT;
+
+    SET rv = '';
+    SET cm = '';
+    SET ch = GivenID;
+    WHILE ch > 0 DO
+        SELECT IFNULL(parent_id,-1) INTO ch FROM
+        (SELECT parent_id FROM organization WHERE id = ch) A;
+        IF ch > 0 THEN
+            SET rv = CONCAT(rv,cm,ch);
+            SET cm = ',';
+        END IF;
+    END WHILE;
+    RETURN rv;
+END$$
+DELIMITER ;
+
+DROP FUNCTION IF EXISTS `GetFamilyTree`;
+DELIMITER $$
+CREATE FUNCTION `GetFamilyTree`(`GivenID` INT) RETURNS varchar(1024) CHARSET utf8
+    NOT DETERMINISTIC
+    READS SQL DATA
+    SQL SECURITY INVOKER
+BEGIN
+
+    DECLARE rv,q,queue,queue_children VARCHAR(1024);
+    DECLARE queue_length,front_id,pos INT;
+
+    SET rv = '';
+    SET queue = GivenID;
+    SET queue_length = 1;
+
+    WHILE queue_length > 0 DO
+        SET front_id = FORMAT(queue,0);
+        IF queue_length = 1 THEN
+            SET queue = '';
+        ELSE
+            SET pos = LOCATE(',',queue) + 1;
+            SET q = SUBSTR(queue,pos);
+            SET queue = q;
+        END IF;
+        SET queue_length = queue_length - 1;
+
+        SELECT IFNULL(qc,'') INTO queue_children
+        FROM (SELECT GROUP_CONCAT(id) qc
+        FROM organization WHERE parent_id = front_id) A;
+
+        IF LENGTH(queue_children) = 0 THEN
+            IF LENGTH(queue) = 0 THEN
+                SET queue_length = 0;
+            END IF;
+        ELSE
+            IF LENGTH(rv) = 0 THEN
+                SET rv = queue_children;
+            ELSE
+                SET rv = CONCAT(rv,',',queue_children);
+            END IF;
+            IF LENGTH(queue) = 0 THEN
+                SET queue = queue_children;
+            ELSE
+                SET queue = CONCAT(queue,',',queue_children);
+            END IF;
+            SET queue_length = LENGTH(queue) - LENGTH(REPLACE(queue,',','')) + 1;
+        END IF;
+    END WHILE;
+    RETURN rv;
+END$$
+DELIMITER ;
+
+DROP FUNCTION IF EXISTS `GetParentIDByID`;
+DELIMITER $$
+CREATE FUNCTION `GetParentIDByID`(GivenID INT) RETURNS int(11)
+    NOT DETERMINISTIC
+    READS SQL DATA
+    SQL SECURITY INVOKER
+BEGIN
+    DECLARE rv INT;
+
+    SELECT IFNULL(parent_id,-1) INTO rv FROM
+    (SELECT parent_id FROM organization WHERE id = GivenID) A;
+    RETURN rv;
+END$$
+DELIMITER ;
+
+DROP FUNCTION IF EXISTS `GetInitials`;
+DELIMITER $$
+CREATE FUNCTION `GetInitials`(str text, expr text) RETURNS text CHARSET utf8
+    NOT DETERMINISTIC
+    READS SQL DATA
+    SQL SECURITY INVOKER
+BEGIN
+    declare result text default '';
+    declare buffer text default '';
+    declare i int default 1;
+    if(str is null) then
+        return null;
+    end if;
+    set buffer = trim(str);
+    while i <= length(buffer) do
+        if substr(buffer, i, 1) regexp expr then
+            set result = concat( result, substr( buffer, i, 1 ));
+            set i = i + 1;
+            while i <= length( buffer ) and substr(buffer, i, 1) regexp expr do
+                set i = i + 1;
+            end while;
+            while i <= length( buffer ) and substr(buffer, i, 1) not regexp expr do
+                set i = i + 1;
+            end while;
+        else
+            set i = i + 1;
+        end if;
+    end while;
+    return result;
+END$$
+DELIMITER ;
+
+DROP FUNCTION IF EXISTS `GetAcronym`;
+DELIMITER $$
+CREATE FUNCTION `GetAcronym`(str text) RETURNS text CHARSET utf8
+    NOT DETERMINISTIC
+    READS SQL DATA
+    SQL SECURITY INVOKER
+BEGIN
+    declare result text default '';
+    set result = GetInitials( str, '[[:alnum:]]' );
+    return result;
+END$$
+DELIMITER ;

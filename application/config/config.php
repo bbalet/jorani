7ea7653b9a3f00f3c41543a58f2ae5c7b9422074
Nito Martinez <Nito@Qindel.ES>
2019-02-15 15:48:23 +0100

    Do not change default settings in config.php

--- application/config/config.php
+++ application/config/config.php
@@ -486,7 +486,7 @@ $config['fonts'] =
 //____________________________________________________________________________
 // Hide/Disable features
 $config['disable_overtime'] = FALSE; //Set this value to TRUE if you want to hide the menu entries related to overtime
-$config['hide_global_cals_to_users'] = TRUE; //Set this value to TRUE if you want to hide global calendars (global/tabular) to users
+$config['hide_global_cals_to_users'] = FALSE; //Set this value to TRUE if you want to hide global calendars (global/tabular) to users
 $config['disable_department_calendar'] = FALSE; //Set this value to TRUE in order to disable the menu entry 'departement'
 $config['disable_workmates_calendar'] = FALSE; //Set this value to TRUE in order to disable the menu entry 'my workmates'
 

21a365b6916456f1eee1fb43b135ec1dd79af9c7
bbalet <benjamin.balet@gmail.com>
2020-10-11 18:51:34 +0200

    BF: GetFamilyTree procedure working with MySQL5.6

---
+++ sql/GetFamilyTree_mysql5.6.sql
@@ -0,0 +1,57 @@
+-- ---------------------------------------------------
+-- GetFamilyTree procedure working with MySQL5.6
+--
+-- @license    http://opensource.org/licenses/AGPL-3.0 AGPL-3.0
+-- @copyright  Copyright (c) 2014-2021 Benjamin BALET
+
+DROP FUNCTION IF EXISTS `GetFamilyTree`;
+DELIMITER $$
+CREATE FUNCTION `GetFamilyTree`(`GivenID` INT) RETURNS varchar(1024) CHARSET utf8
+    NOT DETERMINISTIC
+    READS SQL DATA
+    SQL SECURITY INVOKER
+BEGIN
+
+    DECLARE rv,q,queue,queue_children VARCHAR(1024);
+    DECLARE queue_length,front_id,pos INT;
+
+    SET rv = '';
+    SET queue = GivenID;
+    SET queue_length = 1;
+
+    WHILE queue_length > 0 DO
+        SET front_id = FORMAT(queue,0);
+        IF queue_length = 1 THEN
+            SET queue = '';
+        ELSE
+            SET pos = LOCATE(',',queue) + 1;
+            SET q = SUBSTR(queue,pos);
+            SET queue = q;
+        END IF;
+        SET queue_length = queue_length - 1;
+
+        SELECT IFNULL(qc,'') INTO queue_children
+        FROM (SELECT GROUP_CONCAT(id) qc
+        FROM organization WHERE parent_id = front_id) A;
+
+        IF LENGTH(queue_children) = 0 THEN
+            IF LENGTH(queue) = 0 THEN
+                SET queue_length = 0;
+            END IF;
+        ELSE
+            IF LENGTH(rv) = 0 THEN
+                SET rv = queue_children;
+            ELSE
+                SET rv = CONCAT(rv,',',queue_children);
+            END IF;
+            IF LENGTH(queue) = 0 THEN
+                SET queue = queue_children;
+            ELSE
+                SET queue = CONCAT(queue,',',queue_children);
+            END IF;
+            SET queue_length = LENGTH(queue) - LENGTH(REPLACE(queue,',','')) + 1;
+        END IF;
+    END WHILE;
+    RETURN rv;
+END$$
+DELIMITER ;

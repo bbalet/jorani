0e5e73e95b0eccab6d901e433f08a14e20e4fd6e
Maithyly SIVAPALAN <47674517+msivapal@users.noreply.github.com>
2021-10-02 18:06:54 +0200

    Create extra_lang.php

---
+++ application/language/russian/extra_lang.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Translation file
+ * @copyright  Copyright (c) 2014-2019 Benjamin BALET
+ * @license      http://opensource.org/licenses/AGPL-3.0 AGPL-3.0
+ * @link            https://github.com/bbalet/jorani
+ * @since         0.4.2
+ * @author        Oleg Koptev, Yevhen Kyrylchenko
+ */
+
+$lang['extra_index_title'] = 'Мои запросы на сверхурочную работу';
+$lang['extra_index_thead_tip_view'] = 'просмотреть';
+$lang['extra_index_thead_tip_edit'] = 'редактировать';
+$lang['extra_index_thead_tip_delete'] = 'удалить';
+$lang['extra_index_thead_id'] = 'ID';
+$lang['extra_index_thead_date'] = 'Дата';
+$lang['extra_index_thead_cause'] = 'Причина';
+$lang['extra_index_thead_duration'] = 'Продолжительность';
+$lang['extra_index_thead_status'] = 'Состояние';
+$lang['extra_index_button_export'] = 'Экспортировать список';
+$lang['extra_index_button_create'] = 'Новый запрос';
+$lang['extra_index_popup_delete_title'] = 'Удалить запрос на сверхурочную работу';
+$lang['extra_index_popup_delete_message'] = 'You are about to delete one overtime request, this procedure is irreversible.';
+$lang['extra_index_popup_delete_question'] = 'Продолжить?';
+$lang['extra_index_popup_delete_button_yes'] = 'Да';
+$lang['extra_index_popup_delete_button_no'] = 'Нет';
+$lang['extra_create_title'] = 'Отправить запрос на сверхурочную работу';
+$lang['extra_create_field_date'] = 'Дата';
+$lang['extra_create_field_duration'] = 'Продолжительность';
+$lang['extra_create_field_duration_description'] = 'В целых днях или дробных частях дня (например 0,125 для 1 часа, при 8-часовом рабочем дне)';
+$lang['extra_create_field_cause'] = 'Причина';
+$lang['extra_create_field_status'] = 'Состояние';
+$lang['extra_create_button_create'] = 'Запрос на сверхурочную работу';
+$lang['extra_create_button_cancel'] = 'Отмена';
+$lang['extra_create_msg_success'] = 'Запрос на сверхурочную работу успешно создан';
+$lang['extra_create_msg_error'] = 'Запрос на сверхурочную работу успешно создан или обновлен, но у вас нет руководителя.';
+$lang['extra_create_mandatory_js_msg'] = '"Поле " + fieldname + " является обязательным."';
+$lang['extra_edit_title'] = 'Редактировать запрос на сверхурочную работу №';
+$lang['extra_edit_hmtl_title'] = 'Редактировать запрос на сверхурочную работу';
+$lang['extra_edit_field_date'] = 'Дата';
+$lang['extra_edit_field_duration'] = 'Продолжительность';
+$lang['extra_edit_field_duration_description'] = 'В целых днях или дробных частях дня (например 0,125 для 1 часа, при 8-часовом рабочем дне)';
+$lang['extra_edit_field_cause'] = 'Причина';
+$lang['extra_edit_field_status'] = 'Состояние';
+$lang['extra_edit_button_update'] = 'Обновить запрос на сверхурочную работу';
+$lang['extra_edit_button_cancel'] = 'Отмена';
+$lang['extra_edit_msg_error'] = 'Невозможно изменить уже отправленный запрос на сверхурочную работу';
+$lang['extra_edit_msg_success'] = 'Запрос на сверхурочную работу успешно обновлен';
+$lang['extra_delete_msg_success'] = 'Запрос на сверхурочную работу успешно удален';
+$lang['extra_delete_msg_error'] = 'You can\'t delete this overtime request';
+$lang['extra_view_title'] = 'Просмотреть запрос на сверхурочную работу №';
+$lang['extra_view_hmtl_title'] = 'Просмотреть запрос на сверхурочную работу';
+$lang['extra_view_field_date'] = 'Дата';
+$lang['extra_view_field_duration'] = 'Продолжительность';
+$lang['extra_view_field_cause'] = 'Причина';
+$lang['extra_view_field_status'] = 'Состояние';
+$lang['extra_view_button_edit'] = 'Редактировать';
+$lang['extra_view_button_back_list'] = 'Вернуться к списку';
+$lang['extra_export_title'] = 'Список запросов на сверхурочную работу';
+$lang['extra_export_thead_id'] = 'ID';
+$lang['extra_export_thead_date'] = 'Дата';
+$lang['extra_export_thead_duration'] = 'Продолжительность';
+$lang['extra_export_thead_cause'] = 'Причина';
+$lang['extra_export_thead_status'] = 'Состояние';

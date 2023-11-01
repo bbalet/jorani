a6ca37a0960f524227eee582cbf64a78846a26a8
Alexandre <webmaster@alexandre.re>
2023-11-01 11:47:05 +0400

    feat: remove GA import in login and header.

--- application/views/session/login_simple.php
+++ application/views/session/login_simple.php
@@ -44,20 +44,6 @@ if (!is_null($fonts)) {
         }
     } ?>
 </style>
-<?php if ($this->config->item('ga_code') != "") { ?>
-    <script>
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-        <?php if (($this->session->userdata('id') != FALSE) && ($this->config->item('ga_code') != FALSE)){
-          $gacode = "ga('create', '%s', { 'userId': '%s' });";
-          echo sprintf($gacode, $this->config->item('ga_code'), $this->session->userdata('id'));
-        }?>
-        ga('create', '<?php echo $this->config->item('ga_code');?>', 'auto');
-        ga('send', 'pageview');
-    </script>
-<?php } ?>
 </head>
 <body>
 
